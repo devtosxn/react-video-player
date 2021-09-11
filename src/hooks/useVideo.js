7298a9c355b1db29988b1b6bc65e89011a1e0ec5
Tosin Ayodele <devtosxn@gmail.com>
2021-09-12 00:08:52 +0100

    add video progress helper functions

--- src/hooks/useVideo.js
+++ src/hooks/useVideo.js
@@ -23,6 +23,28 @@ const useVideo = (videoElement) => {
       ? videoElement.current.play()
       : videoElement.current.pause();
   }, [playerState.isPlaying, videoElement]);
+
+  // Determine the current progress of the video
+  // Objective is to show this on the progress bar
+  const handleProgress = () => {
+    const progress =
+      (videoElement.current.currentTime / videoElement.current.duration) * 100;
+    setPlayerState({
+      ...playerState,
+      progress,
+    });
+  };
+
+  // Provide the functionality to drag the progress bar so that choose the current time of the video to view
+  const handleVideoProgress = (e) => {
+    const manualChange = Number(e.target.value);
+    const newTime = manualChange * (videoElement.current.duration / 100);
+    videoElement.current.currentTime = newTime;
+    setPlayerState({
+      ...playerState,
+      progress: manualChange,
+    });
+  };
 };
 
 export default useVideo;
