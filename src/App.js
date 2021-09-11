43e41dca6c2de5f92f66725628cb3f4f8b114084
Tosin Ayodele <devtosxn@gmail.com>
2021-09-12 00:30:51 +0100

    add basic layout for video and render on page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useRef } from "react";
 import "./App.css";
 
+import video from "./assets/video.mp4";
+import useVideo from "./hooks/useVideo";
+
 function App() {
-  return <div className="App">Hello World!</div>;
+  const videoElement = useRef(null);
+  const {
+    playerState,
+    togglePlay,
+    handleTimeUpdate,
+    handleVideoProgress,
+    handleVideoSpeed,
+    toggleMute,
+  } = useVideo(videoElement);
+  return (
+    <div className="container">
+      <div className="video-wrapper">
+        <video src={video} ref={videoElement} onTimeUpdate={handleTimeUpdate} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
